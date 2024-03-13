c4c9bb16b812f61b2af7a3d60bdde47dad1733e6
Simone G <simonegordon12@icloud.com>
2024-03-13 11:34:28 +0000

    Files needed to run enzo in BenProblem3D dir

---
+++ run/Hydro/Hydro-3D/BenProblem3D/enzo_env.sh
@@ -0,0 +1,2 @@
+module load hdf5/gcc-11/mpich/1.14.0
+export LD_LIBRARY_PATH=/home/sgordon/software/local/lib:$LD_LIBRARY_PATH
---
+++ run/Hydro/Hydro-3D/BenProblem3D/yt_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/brs/tmox/tmox_conda
+tmox_activate yt-dev
+conda activate yt
